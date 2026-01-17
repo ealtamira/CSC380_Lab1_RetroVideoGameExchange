509e5842dbede562a0a3ff004d4d6d0b9faf9cf0
EAltamirano <ealtamira705@gmail.com>
2026-01-16 23:35:47 -0700

    Initial CSC380 Lab 1 submission

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use("/auth", require("./routes/auth.routes"));
+app.use("/users", require("./routes/users.routes"));
+app.use("/games", require("./routes/games.routes"));
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    error: "Server Error",
+    message: err.message
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
