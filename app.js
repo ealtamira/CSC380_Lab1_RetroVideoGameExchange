20026c1e61358abc636d4141b6ad83119ee9fb15
EAltamirano <ealtamira705@gmail.com>
2026-01-23 12:43:02 -0700

    Fix spelling errors

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const PORT = 3000;
 app.use(express.json());
 
 app.use("/auth", require("./routes/auth.routes"));
-app.use("/users", require("./routes/users.routes"));
-app.use("/games", require("./routes/games.routes"));
+app.use("/users", require("./routes/user.routes"));
+app.use("/games", require("./routes/game.routes"));
 
 app.use((err, req, res, next) => {
   res.status(500).json({
